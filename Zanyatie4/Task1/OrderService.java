6f6f1e5afcebd1c3810812980c8ba00293ea1c7a
Aliaksandr Kavalenka <aleksandr.kovalenko007@gmail.com>
2018-07-11 13:34:11 +0000

    Update OrderService.java

--- Zanyatie4/Task1/OrderService.java
+++ Zanyatie4/Task1/OrderService.java
@@ -5,29 +5,80 @@ import Zanyatie4.Task1.entity.Order;
 public class OrderService {
 
     OrderRepository repository = new OrderRepository();
+    
+    // public Order[] getOrders(){
+    //     return repository.getOrders();
+    // }
+    
+    // public void addOrder(Order order) {
+    //     int count = 0;
+    //     for (Order anOrder : repository.getOrders()) {
+    //         if (anOrder != null) {
+    //             count++;
+    //         }
+    //     }
+    //     if (repository.getOrders().length - count < 3) {
+    //         checkSizeOfArray(orders);
+    //     }
+    //     int index = checkNullRow(repository.getOrders());
+    //     repository.getOrders()[index] = order;
+    // }
 
-    public void addOrder(Order order) {
+    // private int checkNullRow(Object[] obj) {
+    //     int count = 0;
+    //     for (Object anObj : obj) {
+    //         if (anObj != null) {
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    
+    public String printCompletedOrdersNew(){
+        for(Order order: orderService.getOrders()){
+            if(order != null){
+                if (order.isCompletedOrder()) {
+                StringBuffer sBuffer = new StringBuffer(order);
+                 sBuffer.append(order);
+                 return sBuffer;
+                }
+            }
+            return "null";
+        }
+    }
+    
+    public void addOrder(Calendar dateOfStartedOrder, int bookId){
+        
         int count = 0;
-        for (Order anOrder : repository.getOrders()) {
+        for (Order anOrder : orders) {
             if (anOrder != null) {
                 count++;
             }
         }
-//        if (repository.getOrders().length - count < 3) {
-//            checkSizeOfArray(orders);
-//        }
-        int index = checkNullRow(repository.getOrders());
-        repository.getOrders()[index] = order;
+        if (orders.length - count < 3) {
+            checkSizeOfArray(orders);
+        }
+        int index = checkNullRow(orders);
+        orders[index] = new Order(books.getBooks[bookId]); //нужна книга для ордера
+       
     }
-
-    private int checkNullRow(Object[] obj) {
-        int count = 0;
-        for (Object anObj : obj) {
-            if (anObj != null) {
-                count++;
+    
+    
+    
+    
+    
+    
+    public Strring printCompletedOrdersSortedByDateOfPeriod(GregorianCalendar startDate, GregorianCalendar endDate) {
+        for (Order anOrder : orders) {
+            if (anOrder != null) {
+                if (anOrder.getDateOfCompletedOrder().after(startDate) & anOrder.getDateOfCompletedOrder().before(endDate)) {
+                    System.out.println(anOrder);
+                    return string;
+                }
             }
         }
-        return count;
     }
+    
+    
 
 }
