4474a492a425ddf6c4bcfe252476e196d7032530
AleksandrKovalenko <alexqw007@gmail.com>
2018-09-07 14:05:43 +0300

    upd

--- util/src/main/java/com/senla/util/dataworker/DataWorker.java
+++ util/src/main/java/com/senla/util/dataworker/DataWorker.java
@@ -18,20 +18,24 @@ public class DataWorker implements IDataWorker {
 
     public void writeDataToFile(IService service, List list) {
         String path = "";
-        Object obj = list.get(0);
-        Class<?> clazz = obj.getClass();
-        String nameClass = clazz.getSimpleName();
-        if (nameClass.equals(Book.class.getSimpleName())) {
-            path = PATH_BOOK_DATA_TEST;
-        } else if (nameClass.equals(Order.class.getSimpleName())) {
-            path = PATH_ORDER_DATA_TEST;
-        } else if (nameClass.equals(Request.class.getSimpleName())) {
-            path = PATH_REQUEST_DATA_TEST;
-        }
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
-            oos.writeObject(list);
-        } catch (IOException e) {
-            log.error("Ошибка записи файла " + e);
+        try {
+            Object obj = list.get(0);
+            Class<?> clazz = obj.getClass();
+            String nameClass = clazz.getSimpleName();
+            if (nameClass.equals(Book.class.getSimpleName())) {
+                path = PATH_BOOK_DATA_TEST;
+            } else if (nameClass.equals(Order.class.getSimpleName())) {
+                path = PATH_ORDER_DATA_TEST;
+            } else if (nameClass.equals(Request.class.getSimpleName())) {
+                path = PATH_REQUEST_DATA_TEST;
+            }
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+                oos.writeObject(list);
+            } catch (IOException e) {
+                log.error("Ошибка записи файла - " + e);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            log.error("Получен пустой список для записи - " + e);
         }
     }
 
@@ -40,7 +44,7 @@ public class DataWorker implements IDataWorker {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
             temp = (List) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            log.error("Ошибка чтения файла " + e);
+            log.error("Ошибка чтения файла - " + e);
         }
         return temp;
     }
